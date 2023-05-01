bebf2b5afad1cd7cd9135c9eea919f66f88dd4eb
Yuri Claro <yuri.claro@outlook.com>
2023-05-01 16:04:40 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO tb_worker (NAME, daily_income) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (NAME, daily_income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (NAME, daily_income) VALUES ('Alex', 250.0);
