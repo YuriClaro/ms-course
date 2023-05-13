73fbff530eb77241386dcc7d30a8b9aa17ca3393
Yuri Claro <yuri.claro@outlook.com>
2023-05-13 16:24:17 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/service/PaymentService.java
@@ -1,13 +1,33 @@
 package com.devsuperior.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	// Orientação para a Requisição na Web
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		// HashMap para poder resgatar o "id" e funcionar na formula do restTemplate.getForObjects
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);		
+		
+		// Requisição para uma API externa usando o RestTemplate
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
